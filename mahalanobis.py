ab156e0b63089a02631aad5d9348b137ece895e0
tmasthay <tyler@ices.utexas.edu>
2022-11-29 10:29:55 -0600

    Add random walk example

--- mahalanobis.py
+++ mahalanobis.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 def mahalanobis(f):
     mean = sum(f) / len(f)
@@ -18,7 +19,38 @@ def mahalanobis_func(f,g):
     m2 = mahalanobis(g)
     return m1(g), m2(f)
 
+def brownian_motion(mu, sig):
+    def helper(N):
+        x = np.zeros(N)
+        x[0] = mu
+        for i in range(1,N):
+            x[i] = x[i-1] + np.random.normal(mu,sig)
+        return x
+    return helper
+
 if( __name__ == "__main__" ):
-    print('MAIN')
+    mu = 0.0
+    sig = 1.0
+    N = 100
+   
+    buffer = 1.5
+    delta = buffer * sig * np.sqrt(N)
+    a = mu - delta
+    b = mu + delta
+    x = np.linspace(a,b,N)
+ 
+    generator = brownian_motion(mu,sig)
+    walk1 = generator(N)
+    walk2 = generator(N)
+    
+    m1,m2 = mahalanobis_func(walk1, walk2)
+
+    plt.plot(x, walk1, label='Walk1')
+    plt.plot(x, walk2, label='Walk2')
+    plt.plot(x, m1, label='M12')
+    plt.plot(x, m2, label='M21')
+    plt.plot(x, m1+m2, label='Msum')
+    plt.title('mu=%f, sig=%f'%(mu,sig))
+    plt.savefig('walk.pdf')
                 
     
