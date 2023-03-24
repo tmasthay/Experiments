d612cf6db434fbd42bed3320b7e4c438cf3290d2
tmasthay <tyler@ices.utexas.edu>
2023-03-23 22:06:12 -0500

    Add unit tests

--- textbook_problems/reverse_linked_list/reverse_linked_list.py
+++ textbook_problems/reverse_linked_list/reverse_linked_list.py
@@ -1,9 +1,47 @@
-from typing import Optional
+from typing import Optional,List
 
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 
+# Helper function to convert a list to a linked list
+def list_to_linked_list(lst: List[int]) -> Optional[ListNode]:
+    if not lst:
+        return None
+    head = ListNode(lst[0])
+    current = head
+    for val in lst[1:]:
+        current.next = ListNode(val)
+        current = current.next
+    return head
+
+# Helper function to convert a linked list to a list
+def linked_list_to_list(head: Optional[ListNode]) -> List[int]:
+    lst = []
+    current = head
+    while current:
+        lst.append(current.val)
+        current = current.next
+    return lst
+
 def reverse_linked_list(head: Optional[ListNode]) -> Optional[ListNode]:
-    return None
+    def helper(h: Optional[ListNode], runner: Optional[ListNode]):
+        if( h is None ):
+            return runner
+        
+        next_node = h.next
+        h.next = runner
+        return helper(next_node, h)
+    return helper(head, None)
+
+
+if( __name__ == "__main__" ):
+    input_list = [1, 2, 3, 4, 5]
+    expected_output_list = [5, 4, 3, 2, 1]
+
+    input_linked_list = list_to_linked_list(input_list)
+    reversed_linked_list = reverse_linked_list(input_linked_list)
+    reversed_list = linked_list_to_list(reversed_linked_list)
+
+    print(reversed_list)
--- textbook_problems/reverse_linked_list/test_reverse_linked_list.py
+++ textbook_problems/reverse_linked_list/test_reverse_linked_list.py
@@ -2,26 +2,6 @@ import pytest
 from typing import Optional, List
 from reverse_linked_list import *
 
-# Helper function to convert a list to a linked list
-def list_to_linked_list(lst: List[int]) -> Optional[ListNode]:
-    if not lst:
-        return None
-    head = ListNode(lst[0])
-    current = head
-    for val in lst[1:]:
-        current.next = ListNode(val)
-        current = current.next
-    return head
-
-# Helper function to convert a linked list to a list
-def linked_list_to_list(head: Optional[ListNode]) -> List[int]:
-    lst = []
-    current = head
-    while current:
-        lst.append(current.val)
-        current = current.next
-    return lst
-
 def test_reverse_linked_list():
     input_list = [1, 2, 3, 4, 5]
     expected_output_list = [5, 4, 3, 2, 1]
@@ -50,4 +30,60 @@ def test_reverse_single_node_linked_list():
     reversed_linked_list = reverse_linked_list(input_linked_list)
     reversed_list = linked_list_to_list(reversed_linked_list)
 
+    assert reversed_list == expected_output_list
+
+def test_reverse_linked_list_1():
+    input_list = [1, 2, 3, 4, 5]
+    expected_output_list = [5, 4, 3, 2, 1]
+    input_linked_list = list_to_linked_list(input_list)
+    reversed_linked_list = reverse_linked_list(input_linked_list)
+    reversed_list = linked_list_to_list(reversed_linked_list)
+    assert reversed_list == expected_output_list
+
+def test_reverse_linked_list_2():
+    input_list = [10, 20, 30]
+    expected_output_list = [30, 20, 10]
+    input_linked_list = list_to_linked_list(input_list)
+    reversed_linked_list = reverse_linked_list(input_linked_list)
+    reversed_list = linked_list_to_list(reversed_linked_list)
+    assert reversed_list == expected_output_list
+
+def test_reverse_linked_list_3():
+    input_list = [1, 1, 1, 1, 1]
+    expected_output_list = [1, 1, 1, 1, 1]
+    input_linked_list = list_to_linked_list(input_list)
+    reversed_linked_list = reverse_linked_list(input_linked_list)
+    reversed_list = linked_list_to_list(reversed_linked_list)
+    assert reversed_list == expected_output_list
+
+def test_reverse_linked_list_4():
+    input_list = [1, 2, 1, 2, 1]
+    expected_output_list = [1, 2, 1, 2, 1]
+    input_linked_list = list_to_linked_list(input_list)
+    reversed_linked_list = reverse_linked_list(input_linked_list)
+    reversed_list = linked_list_to_list(reversed_linked_list)
+    assert reversed_list == expected_output_list
+
+def test_reverse_linked_list_5():
+    input_list = [1, 2, 3, 2, 1]
+    expected_output_list = [1, 2, 3, 2, 1]
+    input_linked_list = list_to_linked_list(input_list)
+    reversed_linked_list = reverse_linked_list(input_linked_list)
+    reversed_list = linked_list_to_list(reversed_linked_list)
+    assert reversed_list == expected_output_list
+
+def test_reverse_linked_list_6():
+    input_list = [7]
+    expected_output_list = [7]
+    input_linked_list = list_to_linked_list(input_list)
+    reversed_linked_list = reverse_linked_list(input_linked_list)
+    reversed_list = linked_list_to_list(reversed_linked_list)
+    assert reversed_list == expected_output_list
+
+def test_reverse_linked_list_7():
+    input_list = [1, 2]
+    expected_output_list = [2, 1]
+    input_linked_list = list_to_linked_list(input_list)
+    reversed_linked_list = reverse_linked_list(input_linked_list)
+    reversed_list = linked_list_to_list(reversed_linked_list)
     assert reversed_list == expected_output_list
