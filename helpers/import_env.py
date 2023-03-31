60b5dd19e684cd0cd5e280c1436455b865c5ba1b
tmasthay <tyler@ices.utexas.edu>
2023-03-31 14:39:47 -0700

    Error handle when folders not found

--- helpers/import_env.py
+++ helpers/import_env.py
@@ -1,8 +1,10 @@
 from subprocess import check_output as co
-import os
 
 def sco(s,split=True):
-    res = co(s,shell=True).decode('utf-8')
+    try:
+        res = co(s,shell=True).decode('utf-8')
+    except:
+        raise
     if( split ):
         return res.split('\n')[:-1]
     else:
@@ -10,7 +12,10 @@ def sco(s,split=True):
     
 def get_subfolders(path, omissions=[], depth=1):
     omissions = [path + '/%s'%e if '/' not in e else e for e in omissions]
-    u = sco(r'find %s -type d -depth %d | grep -v "\/[_.]"'%(path, depth))
+    try:
+        u = sco(r'find %s -type d -depth %d | grep -v "\/[_.]"'%(path, depth))
+    except:
+        u = []
     if( len(omissions) > 0 ):
         u = [e for e in u if e not in omissions]
     return u
@@ -22,13 +27,3 @@ def get_submodules(path):
 
 def get_local_name(s,ext='.py'):
     return s if '/' not in s else s.split('/')[-1].replace(ext, '')
-
-if( __name__ == "__main__" ):
-    u = get_subfolders(os.getcwd(), omissions=['examples'])
-    v = get_submodules(os.getcwd())
-
-    u = [get_local_name(e) for e in u]
-    v = [get_local_name(e) for e in v]
-
-    print(u)
-    print(v)
