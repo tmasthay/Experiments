ee831baab9ff731d72d485f4185f6ae2d10a5580
tmasthay <tyler@ices.utexas.edu>
2023-04-13 00:12:16 -0500

    Update cinclude to place result in clipboard

--- helpers/global_helpers.py
+++ helpers/global_helpers.py
@@ -1,4 +1,5 @@
 from subprocess import check_output as co
+import os
 
 #global helpers tyler
 class ght:
@@ -7,3 +8,11 @@ class ght:
         if( split ):
             u = u.split('\n')[:-1]
         return u
+
+    def conda_include_everything():
+        inc_paths = ':'.join(ght.sco('find $CONDA_PREFIX/include -type d'))
+        c_path = os.environ("C_INCLUDE_PATH")
+        cmd = "echo 'export C_INCLUDE_PATH=%s:%s'"%(c_path, inc_paths)
+        cmd += ' | pbcopy'
+        os.system(cmd) 
+        
