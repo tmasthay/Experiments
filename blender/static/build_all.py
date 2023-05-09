6aed22019b409f3aeb4923b492af6f88731e9fe3
tmasthay <tyler@ices.utexas.edu>
2023-05-09 18:02:37 -0400

    Simplify build_all

--- blender/static/build_all.py
+++ blender/static/build_all.py
@@ -1,11 +1,4 @@
-import os, sys, bpy
-
-# Get the path to the directory containing your script files
-script_directory = os.path.dirname(os.path.abspath(__file__))
-
-# Add the directory to the Python path
-if script_directory not in sys.path:
-    sys.path.append(script_directory)
+import bpy
 
 main_drum = bpy.data.texts['main_drum'].as_module()
 fast_drum = bpy.data.texts['fast_drum'].as_module()
