50e83090d8f185a108604d9fadf1a3a719eb81ec
tmasthay <tyler@ices.utexas.edu>
2023-05-09 18:10:16 -0400

    Fix syntax error on build_all

--- blender/static/build_all.py
+++ blender/static/build_all.py
@@ -1,7 +1,7 @@
 import bpy
 
-main_drum = bpy.data.texts['main_drum'].as_module()
-fast_drum = bpy.data.texts['fast_drum'].as_module()
+main_drum = bpy.data.texts['main_drum.py'].as_module()
+fast_drum = bpy.data.texts['fast_drum.py'].as_module()
 
 fast_drum.build_fast_drum()
 main_drum.build_main_drum()
