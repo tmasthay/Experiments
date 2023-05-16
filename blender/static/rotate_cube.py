0b90f0b429d6f1d26c02dee0ee518245c8f7bc93
tmasthay <tyler@ices.utexas.edu>
2023-05-15 23:53:12 -0500

    Add rotate_cube

---
+++ blender/static/rotate_cube.py
@@ -0,0 +1,51 @@
+import bpy
+import numpy as np
+from math import radians
+ah = bpy.data.texts['animation_helpers.py'].as_module()
+
+def sync_rotation(theta, axis):
+    cube = bpy.data.objects['Cube']
+    cube.rotation_euler = (0,0,0)
+    delta = tuple([theta*e for e in axis])
+
+    frames_per_beat = 16
+    d = int(frames_per_beat / 4)
+    spacing = list(d * np.array([
+        3, 3, 3, 3, 3,
+        2, 1, 3, 3, 3,
+        3, 2
+    ]))
+    start_frame = 532
+    num_copies = 100
+    def controller():
+        def helper(obj, frame_no, i):
+            obj.keyframe_insert(
+                data_path="rotation_euler", 
+                frame=(frame_no-1)
+            )
+            
+            obj.rotation_euler = tuple([
+                delta[j] + obj.rotation_euler[j] for j in range(3)
+            ])
+            obj.keyframe_insert(
+                data_path="rotation_euler", 
+                frame=frame_no
+            )
+            obj.keyframe_insert(
+                data_path="rotation_euler",
+                frame=(frame_no+1)
+            )
+        return helper
+    
+    ah.place_spaced_sequence(
+        cube,
+        spacing,
+        start_frame,
+        num_copies,
+        controller()
+    )
+
+def build_spinning_lasers():
+    theta = 1.0
+    axis = (0,0,1)
+    sync_rotation(theta, axis)
