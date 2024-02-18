9c3bce0fc994dd8c9bc6004b57b3a9a2a6b7d450
tmasthay <tyler@ices.utexas.edu>
2024-02-17 23:53:24 -0600

    FEATURE: Add current output directory

--- hydra_evangelizing/cfg_within_cfg/main.py
+++ hydra_evangelizing/cfg_within_cfg/main.py
@@ -2,9 +2,14 @@ import hydra
 from omegaconf import DictConfig
 
 
+def out_dir():
+    return hydra.core.hydra_config.HydraConfig.get().runtime.output_dir
+
+
 @hydra.main(config_path="cfg", config_name="cfg", version_base=None)
 def main(cfg: DictConfig) -> None:
     print(cfg)
+    print(f'Current out_dir: {out_dir()}')
 
 
 if __name__ == "__main__":
