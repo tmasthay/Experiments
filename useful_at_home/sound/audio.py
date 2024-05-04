60a9f0393ae28cf9ad25cd87ed7bf3581dc9adce
tmasthay <tyler@ices.utexas.edu>
2024-05-04 18:40:42 -0500

    FEATURE: unsure -- eveyrthing works now

--- useful_at_home/sound/audio.py
+++ useful_at_home/sound/audio.py
@@ -8,19 +8,25 @@ import os
 @hydra.main(config_path="cfg", config_name="cfg")
 def play_audio(cfg: DictConfig) -> None:
     # Load the audio file
-    file_path = os.path.join(os.path.dirname(__file__), cfg.audio.file_path)
+    file_path = os.path.join(os.path.dirname(__file__), cfg.file_path)
     audio = AudioSegment.from_file(os.path.abspath(file_path))
     
     # Calculate the total number of iterations to play
-    max_iters = int(cfg.audio.total_time / cfg.audio.interval)
+    max_iters = int(cfg.total_time / cfg.interval)
     
     # Segment to be played
-    segment = audio[cfg.audio.start * 1000:cfg.audio.end * 1000]
+    if type(cfg.end) == str and cfg.start.lower()[0] in ['a']:
+        start = 0
+        end = len(audio)
+    else:
+        start = cfg.start * 1000
+        end = cfg.end * 1000
+    segment = audio[start:end]
     
     # Loop to play the audio segment at the specified intervals
     for _ in range(max_iters):
         play(segment)
-        time.sleep(cfg.audio.interval)
+        time.sleep(cfg.interval)
 
 if __name__ == "__main__":
     play_audio()
