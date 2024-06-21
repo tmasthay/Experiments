32f743f83fe61faa155ad42832de877029b6bee8
tmasthay <tyler@ices.utexas.edu>
2024-06-21 15:45:59 -0500

    EXAMPLE: Dynamic import syntax for configs

---
+++ eat_cfg/tmp.py
@@ -0,0 +1,26 @@
+from mh.core import exec_imports, DotDict
+from misfit_toys.utils import resolve
+
+u = DotDict(
+    dict(
+        a='^^torch.nn',
+        b='^^torch.nn.functional',
+        c='self.a.Linear',
+        d='^^torch.nn|Linear',
+    )
+)
+print(f'After construction: {u}')
+
+v = exec_imports(u)
+
+print(
+    f'u after calling exec_imports and assigning to v: {u}\n\nSide effects! -->'
+    f' {(u==v)=}'
+)
+
+w = resolve(v, relax=False)
+
+print(
+    f'u after call resolve(v,relax=False) and assigning to w\n\n{u=}\n\nSide'
+    f' effects! --> {(u==w)=}'
+)
